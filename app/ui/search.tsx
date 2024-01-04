08052efbb170ed50f6243eb7b2c68ed04c307b68
guanyu <guanyu@mail>
2024-01-04 16:47:39 +0800

    lastpass插件导致的错误

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -22,7 +22,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   }, 300);
 
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <form className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
@@ -36,6 +36,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
+    </form>
   );
 }
